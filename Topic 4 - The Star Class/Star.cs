3ddca9859c0cc8a24a81c02eed6d2efdd11cf988
339784910 <339784910@2260I-L29-010>
2026-04-13 13:19:29 -0400

    star class

---
+++ Topic 4 - The Star Class/Star.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Topic_4___The_Star_Class
+{
+    public class Star
+    {
+        private Rectangle _location;
+        private Vector2 _speed;
+        private Texture2D _texture;
+
+
+
+    }
+}
