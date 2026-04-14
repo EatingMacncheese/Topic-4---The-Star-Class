548641d4a3f220aa30393b8462dc0244215d64a8
339784910 <339784910@2260I-L29-010>
2026-04-14 13:17:16 -0400

    fix location/star.X error later

--- Topic 4 - The Star Class/Game1.cs
+++ Topic 4 - The Star Class/Game1.cs
@@ -1,6 +1,9 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using System;
+using System.Collections.Generic;
+using System.Reflection.Emit;
 
 namespace Topic_4___The_Star_Class
 {
@@ -8,9 +11,11 @@ namespace Topic_4___The_Star_Class
     {
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
-
+        Random generator = new Random();
+        Star star1;
+        List<Star> stars;
         Rectangle window, shipRect;
-        Texture2D shipTexture; 
+        Texture2D shipTexture, starTexture; 
         public Game1()
         {
             _graphics = new GraphicsDeviceManager(this);
@@ -21,8 +26,18 @@ namespace Topic_4___The_Star_Class
         protected override void Initialize()
         {
             // TODO: Add your initialization logic here
+            star1 = new Star(starTexture, new Rectangle(100, 100, 2, 2), new Vector2(-2, 0));
             shipRect = new Rectangle(50, 250, 150, 40);
             window = new Rectangle(0, 0, 800, 600);
+            stars = new List<Star>();
+            for (int i = 0; i < 150; i++)
+            {
+                int x, y;
+                x = generator.Next(window.Width);
+                y = generator.Next(window.Height);
+                Rectangle star = new Rectangle(x, y, 3, 3);
+                stars.Add(new Star(starTexture, star, new Vector2(-2, 0)));
+            }
             _graphics.PreferredBackBufferWidth = window.Width;
             _graphics.PreferredBackBufferHeight = window.Height;
             _graphics.ApplyChanges();
@@ -33,6 +48,7 @@ namespace Topic_4___The_Star_Class
         {
             _spriteBatch = new SpriteBatch(GraphicsDevice);
             shipTexture = Content.Load<Texture2D>("Images/enterprise");
+            
 
             // TODO: use this.Content to load your game content here
         }
@@ -41,9 +57,13 @@ namespace Topic_4___The_Star_Class
         {
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
+            foreach (Star star in stars)
+                star.Update();
+            if (Star.Location.Right < 0)
+                star.X = window.Width;
 
             // TODO: Add your update logic here
-            
+
             base.Update(gameTime);
         }
 
@@ -51,6 +71,9 @@ namespace Topic_4___The_Star_Class
         {
             GraphicsDevice.Clear(Color.Black);
             _spriteBatch.Begin();
+            Star.Draw(_spriteBatch);
+            foreach (var star in stars)
+                star.Draw(_spriteBatch);
             _spriteBatch.Draw(shipTexture, shipRect, Color.White);
             _spriteBatch.End();
             // TODO: Add your drawing code here
--- Topic 4 - The Star Class/Star.cs
+++ Topic 4 - The Star Class/Star.cs
@@ -14,8 +14,48 @@ namespace Topic_4___The_Star_Class
         private Rectangle _location;
         private Vector2 _speed;
         private Texture2D _texture;
+        private Texture2D starTexture;
+        private Microsoft.Xna.Framework.Rectangle rectangle;
+        private Microsoft.Xna.Framework.Vector2 vector2;
+        internal static int X;
 
+        public Star(Texture2D texture, Rectangle location, Vector2 speed)
+        {
+            _location = location;
+            _speed = speed;
+            _texture = texture;
+        }
 
+        public Star(Texture2D starTexture, Microsoft.Xna.Framework.Rectangle rectangle, Microsoft.Xna.Framework.Vector2 vector2)
+        {
+            this.starTexture = starTexture;
+            this.rectangle = rectangle;
+            this.vector2 = vector2;
+        }
 
+        public void Update()
+        {
+            _location.Offset(_speed);
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(_texture, _location, Color.White);
+        }
+        public Rectangle Location
+        {
+            get { return _location; }
+            set { _location = value; }
+        }
+        public int Y
+        {
+            get { return _location.Y; }
+            set { _location.Y = value; }
+        }
+        public Rectangle Location
+        {
+            get { return _location; }
+            set { _location = value; }
+        }
     }
 }
